f59775526e2954c0c29a017a1e5c8bd1ffa5957e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-08-05 09:10:42 +0530

    Update

---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='xl:px-48 md:px-12 px-4 md:py-32'>
+            <div className="flex justify-between">
+                <div className="">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
